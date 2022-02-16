91807d88c4cd88b6896c2ff69e9eee4fe83b20db
Carlos Britto <carlosbritto32@gmail.com>
2022-02-16 11:18:53 -0500

    We create all the components of the project to complete a visual app, using atomic desing

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Header } from "../components/Header";
 import { Layout } from "../containers/layout";
-import { Login } from "../containers/login";
-import { RecoveryPassword } from "../containers/RecoveryPassword";
+import { Login } from "../pages/Login";
+import { RecoveryPassword } from "../pages/RecoveryPassword";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound";
+import { SendEmail } from "../pages/SendEmail";
+import { NewPassword } from "../pages/NewPassword";
+import { MyAccount } from "../pages/MyAccount";
+import { CreateAccount } from "../pages/CreateAccount";
+import { Checkout } from "../pages/Checkout";
+import { Orders } from "../pages/Orders";
 import "../styles/global.scss";
 
 const App = () => {
@@ -16,6 +21,12 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path="/send-email" component={<SendEmail />} />
+          <Route path="/new-password" component={<NewPassword />} />
+          <Route path="/account" component={<MyAccount />} />
+          <Route path="/signup" component={<CreateAccount />} />
+          <Route path="/checkout" component={<Checkout />} />
+          <Route path="/orders" component={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
